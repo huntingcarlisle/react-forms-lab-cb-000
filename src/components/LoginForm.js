34cc4405ca3ce6a6e5150ec86e0a6f597a3d8a16
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-03-02 06:47:23 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" 
+            <input id="username" name="username" type="text"
             value={this.state.username}
             onChange={this.handleInputChange}
             />
@@ -28,7 +28,7 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" 
+            <input id="password" name="password" type="password"
             value={this.state.password}
             onChange={this.handleInputChange}
             />
